ff074a9ef0557ebedd6bc30cd575c8648e2b9263
Alejandro Landeros <alejandro.landeros@bluemediaventures.com>
2017-11-18 21:56:50 -0800

    finished all questions

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -82,8 +82,9 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
-num_iters = 400;
+alpha = 1;
+% alpha = 0.7;
+num_iters = 50;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
@@ -100,11 +101,40 @@ fprintf('Theta computed from gradient descent: \n');
 fprintf(' %f \n', theta);
 fprintf('\n');
 
+% selecting learning rates.
+% 1 is best.
+% hold on;
+% theta = zeros(3, 1);
+% [theta,  J_history] = gradientDescentMulti(X, y,  theta,  1,  num_iters);
+% plot(1:numel(J_history), J_history, '-y', 'LineWidth', 2);
+% fprintf('Theta computed from gradient descent: \n');
+% fprintf(' %f \n', theta);
+% fprintf('\n');
+
+
+% theta = zeros(3, 1);
+% [theta,  J_history] = gradientDescentMulti(X, y,  theta,  alpha/100.0,  num_iters);
+% plot(1:numel(J_history), J_history, '-r', 'LineWidth', 2);
+% fprintf('Theta computed from gradient descent: \n');
+% fprintf(' %f \n', theta);
+% fprintf('\n');
+
+
+% theta = zeros(3, 1);
+% [theta,  J_history] = gradientDescentMulti(X, y,  theta,  alpha/1000.0,  num_iters);
+% plot(1:numel(J_history), J_history, '-k', 'LineWidth', 2);
+% fprintf('Theta computed from gradient descent: \n');
+% fprintf(' %f \n', theta);
+% fprintf('\n');
+
+
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+% price = 0; % You should change this
+disp(theta);
+price = [1 (1650 - mu(1))/sigma(1) (3-mu(2))/sigma(2)] * theta;
 
 
 % ============================================================
@@ -149,7 +179,8 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+disp(theta);
+price = [1 1650 3]* theta; % You should change this
 
 
 % ============================================================
--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -31,7 +31,7 @@ for iter = 1:num_iters
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
-    disp(J_history(iter))
+    % disp(J_history(iter))
 end
 
 end
